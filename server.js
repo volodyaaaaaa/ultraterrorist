2c1a4371a2ada444d8e78cb6c25b54afe50c2259
volodyaaaaaa <choresgot@gmail.com>
2022-12-08 13:53:53 +0100

    ADDED JS SERVER

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require ('http')
+let requestCount = 0
+const server = http.createServer((request, response) => {
+   requestCount++
+   switch(request.url) {
+      case '/banword':
+         response.write ('Pidoras , negr , evrey etc')
+         break;
+      case '/banlist':
+         response.write('Mazeloff , jesusAVGN , eeoneguy')
+         break;
+      default:
+         response.write ('404 Not Found , Sorry about that bro(')
+
+   response.write('Vova Junior first back inc' + requestCount)   
+   }
+})
+
+server.listen(2000)
